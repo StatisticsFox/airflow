afcd286bf38979c3dbfe4a48a2f4fbed1319f320
StatisticsFox <akfktl328@naver.com>
2023-11-21 22:53:19 +0900

    email operator 수정

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -5,13 +5,13 @@ from airflow.operators.email import EmailOperator
 
 with DAG(
     dag_id="dags_email_operator",
-    schedule="*/10 * * * *",
+    schedule="0 8 1 * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     send_email_task = EmailOperator(
         task_id= 'send_email_task',
-        to='1234kimlove@naver.com',
-        subject="사랑해",
+        to='akfktl328@naver.com',
+        subject="작업 진행 현황 공지",
         html_content="Airflow 작업을 완료했습니다."
     )
