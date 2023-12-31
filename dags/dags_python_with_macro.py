479b3df298f0d3e165864992489cb5e46ff39412
StatisticsFox <akfktl328@naver.com>
2023-12-31 17:04:46 +0900

    python with macro 파일 주석 수정

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -17,7 +17,7 @@ with DAG(
      }
     )
     def get_datetime_macro(**kwargs):
-        # 위 templates_dict 전체가 key값으로 들어감 
+        # 위 templates_dict라는 string이 key값 나머지 전체가 value값으로 들어감 
         templates_dict = kwargs.get('templates_dict') or {}
         if templates_dict:
             start_date = templates_dict.get('start_date') or 'start_date없음' # key 값이 없으면 스트링으로 없다고 할당
@@ -29,7 +29,8 @@ with DAG(
     @task(task_id='task_direct_calc')
     def get_datetime_calc(**kwargs):
         from dateutil.relativedelta import relativedelta
-
+        # 스케줄러 부하 경감을 위해 패키지 import 하는 부분을 task 안에 명명 주기적으로 문법 검사를 하는 airflow의 부하를 줄일 수 있음
+        # 오퍼레이터 안에만 필요한 함수는 오퍼레이터 안에 선언을 하는게 나중에 신상에 좋음
         data_interval_end = kwargs['data_interval_end']
         prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(months=-1, day=1)
         prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(days=-1)
